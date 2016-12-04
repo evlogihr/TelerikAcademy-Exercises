a13e8cdef9897ff5f241728461c7015ab6abc673
evlogihr <evlogihristov@yahoo.com>
2016-12-04 14:38:08 +0200

    Solution to dancing Moves

---
+++ DancingMoves/DancingMoves.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+
+namespace DancingMove
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] floor = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            string line;
+            int i = 0;
+            long sum = 0;
+            int rounds = 0;
+            while ((line = Console.ReadLine()) != "stop")
+            {
+                string[] split = line.Split(' ');
+                int count = int.Parse(split[0]);
+                int dir = split[1] == "right" ? 1 : -1;
+                int step = int.Parse(split[2]) * dir;
+
+                for (int c = 0; c < count; c++)
+                {
+                    i += step;
+                    while (i < 0)
+                    {
+                        i += floor.Length;
+                    }
+
+                    while (i > floor.Length - 1)
+                    {
+                        i -= floor.Length;
+                    }
+
+                    sum += floor[i];
+                }
+
+                 rounds++;
+            }
+
+            Console.WriteLine("{0:f1}", sum / (double)rounds);
+        }
+    }
+}
